ffed644ea811241cc9685d3d76b1f594cbbed75f
yunyoung1819 <yunyoung1819@hanmail.net>
2024-11-27 21:00:33 +0900

    :sparkles: feat: 메인페이지 구현

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -110,4 +110,11 @@ public class ItemController {
         model.addAttribute("maxPage", 5);
         return "item/itemMng";
     }
+
+    @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId") Long itemId) {
+        ItemFormDto itemFormDto = itemService.getItemDetail(itemId);
+        model.addAttribute("item", itemFormDto);
+        return "item/itemDtl";
+    }
 }
