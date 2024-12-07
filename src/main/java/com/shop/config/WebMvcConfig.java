c1aeb5bb96b23574c95a59f85d922703240192be
yunyoung1819 <yunyoung1819@hanmail.net>
2024-12-07 13:41:51 +0900

    :sparkles: feat: 상품 등록 페이지 구현

--- src/main/java/com/shop/config/WebMvcConfig.java
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -14,6 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations(uploadPath);
+                .addResourceLocations("classpath:/static/images/");
     }
 }
